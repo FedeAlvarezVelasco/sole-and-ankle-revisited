cc0217bc04c97bf8fd7b45b4e0e0667493c2c89b
Fede Alvarez <fede.alvarez.velasco@gmail.com>
2021-12-01 05:48:18 -0600

    Major update to all components. Added responsive functionality and constant colors to globalstyles as css variables.

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -2,36 +2,104 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
-
-import { QUERIES } from '../../constants';
-
+import { WEIGHTS } from '../../constants';
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
-
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+      <Content>
+      <CloseButton onClick={onDismiss}>
+        <Icon id="close" />
+          <VisuallyHidden>
+            Dismisa menu
+          </VisuallyHidden>
+        </CloseButton>
+      <Filler />
+      <Nav>
+        <NavLink href="/sale">Sale</NavLink>
+        <NavLink href="/new">New&nbsp;Releases</NavLink>
+        <NavLink href="/men">Men</NavLink>
+        <NavLink href="/women">Women</NavLink>
+        <NavLink href="/kids">Kids</NavLink>
+        <NavLink href="/collections">Collections</NavLink>
+      </Nav>
+      <Footer>
+        <FooterLink href="/terms">Terms and Conditions</FooterLink>
+        <FooterLink href="/privacy">Privacy Policy</FooterLink>
+        <FooterLink href="/contact">Contact Us</FooterLink>
+      </Footer>
+      </Content>
+    </Overlay>
   );
 };
+
+const Overlay = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: var(--color-backdrop);
+  display: flex;
+  justify-content: flex-end;
+
+`;
+
+const Content = styled(DialogContent)`
+  background: white;
+  height: 100%;
+  width: 300px;
+  padding: 32px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const CloseButton = styled(UnstyledButton)`
+  position: absolute;
+  top: 16px;
+  right: 0px;
+  padding: 16px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const NavLink = styled.a`
+  color: var(--color-gray-900);
+  font-weight: ${WEIGHTS.medium};
+  text-decoration: none;
+  font-size: 1.125rem;
+  text-transform: uppercase;
+
+  &:first-of-type {
+    color: var(--secondar);
+  }
+`;
+
+const Filler = styled.div`
+  flex: 1; 
+`;
+
+const Footer = styled.footer`
+  flex: 1; 
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  gap: 8px;
+`;
+
+const FooterLink = styled.a`
+  color: var(--color-gray-700);
+  font-weight: ${WEIGHTS.normal};
+  text-decoration: none;
+  font-size: 0.8rem;
+  text-transform: capitalize;
+`;
 
 export default MobileMenu;
